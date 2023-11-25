0deeb319ad4c866941db3b26c004eb2920aff847
tryfilter <61294210+tryfilter@users.noreply.github.com>
2023-11-25 21:47:23 +0200

    Remove unneeded guava dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,9 +20,6 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.3")
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-    // This dependency is used by the application.
-    implementation("com.google.guava:guava:32.1.1-jre")
 }
 
 // Apply a specific Java toolchain to ease working on different environments.

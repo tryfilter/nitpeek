0b90e8f9dedf4e767676cc9f9b882ac599752647
tryfilter <61294210+tryfilter@users.noreply.github.com>
2023-12-15 17:42:36 +0200

    Improve doc

--- app/src/main/java/nitpeek/core/api/analyze/analyzer/AnalyzeAcrossLines.java
+++ app/src/main/java/nitpeek/core/api/analyze/analyzer/AnalyzeAcrossLines.java
@@ -7,22 +7,32 @@ import nitpeek.core.api.common.Feature;
 
 import java.util.List;
 
+
 public class AnalyzeAcrossLines implements Analyzer {
 
     private final Analyzer analyzer;
-    private final Transformer transformer = new LineJoiner();
+    private final Transformer lineJoiner = new LineJoiner();
 
+    /**
+     * @param analyzer the analyzer to wrap: the wrapped analyzer will receive all pages passed to the wrapping analyzer
+     *                 as single-line pages; this way, any analysis it performs disregards line separation, without the
+     *                 being aware of this
+     */
     public AnalyzeAcrossLines(Analyzer analyzer) {
         this.analyzer = analyzer;
     }
 
+    /**
+     * @return a list of features produced by the wrapped analyzer, transformed to correctly relate to the pages
+     * originally passed to the wrapping analyzer
+     */
     @Override
     public List<Feature> findFeatures() {
-        return analyzer.findFeatures().stream().map(transformer::transformFeature).toList();
+        return analyzer.findFeatures().stream().map(lineJoiner::transformFeature).toList();
     }
 
     @Override
     public void processPage(TextPage page) {
-        analyzer.processPage(transformer.transformPage(page));
+        analyzer.processPage(lineJoiner.transformPage(page));
     }
 }

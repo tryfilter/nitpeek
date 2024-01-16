c28958a12765b49716da92570ed00cf9e65deec6
tryfilter <61294210+tryfilter@users.noreply.github.com>
2024-01-16 12:02:04 +0200

    Improve doc

--- lib-core/src/main/java/nitpeek/core/api/analyze/Rule.java
+++ lib-core/src/main/java/nitpeek/core/api/analyze/Rule.java
@@ -9,14 +9,14 @@ import nitpeek.core.api.analyze.analyzer.Analyzer;
  * Each Feature has its own name and description which are not necessarily related
  * to the name and description of the rule.
  * A rule has an abstract description of its intent, while a feature has a description specific to the way in
- * which it breaks the rule, potentially including suggested fixes.
+ * which it conforms to the rule, potentially including suggested actions.
  */
 public interface Rule {
 
     RuleType getType();
 
     /**
-     * @return an Analyzer that identifies violations of this Rule<br>
+     * @return an Analyzer that identifies instances conforming to this Rule<br>
      * <br>
      * Note that one Analyzer may identify many different types of Feature.
      */
--- lib-core/src/main/java/nitpeek/core/api/common/FeatureComponent.java
+++ lib-core/src/main/java/nitpeek/core/api/common/FeatureComponent.java
@@ -3,8 +3,8 @@ package nitpeek.core.api.common;
 import java.util.Optional;
 
 /**
- * Most granular description of an issue in a text.<br>
- * It simply describes what is wrong, and where it is in the text.
+ * Most granular description of a feature in a text.<br>
+ * It simply describes a feature or part thereof, and where precisely it is located in the text.
  */
 public interface FeatureComponent {
 

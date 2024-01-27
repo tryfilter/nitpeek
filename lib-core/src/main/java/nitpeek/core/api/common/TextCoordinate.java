e99829b008028b27b404b8fe686a762e75042ee0
tryfilter <61294210+tryfilter@users.noreply.github.com>
2024-01-27 17:58:52 +0200

    Make TextCoordinate comparable

--- lib-core/src/main/java/nitpeek/core/api/common/TextCoordinate.java
+++ lib-core/src/main/java/nitpeek/core/api/common/TextCoordinate.java
@@ -7,7 +7,7 @@ package nitpeek.core.api.common;
  * @param line      the ordinal of the line, 0-based, not negative. This refers to a specific line in the page
  * @param character the ordinal of the character, 0-based, not negative. This refers to a specific character in the line
  */
-public record TextCoordinate(int page, int line, int character) {
+public record TextCoordinate(int page, int line, int character) implements Comparable<TextCoordinate> {
     public TextCoordinate {
         if (page < 0) throw new IllegalArgumentException("page must be >= 0, was " + page);
         if (line < 0) throw new IllegalArgumentException("line must be >= 0, was " + line);
@@ -24,5 +24,16 @@ public record TextCoordinate(int page, int line, int character) {
 
     public TextSelection extendToSelection(int totalPages, int totalLines, int totalCharacters) {
         return new TextSelection(this, new TextCoordinate(page() + totalPages - 1, line() + totalLines - 1, character() + totalCharacters - 1));
+    }
+
+    @Override
+    public int compareTo(TextCoordinate o) {
+        int pageComparison = Integer.compare(page(), o.page());
+        if (pageComparison != 0) return pageComparison;
+
+        int lineComparison = Integer.compare(line(), o.line());
+        if (lineComparison != 0) return lineComparison;
+
+        return Integer.compare(character(), o.character());
     }
 }

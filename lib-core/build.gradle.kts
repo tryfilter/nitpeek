d1c5922292a2cfda9e2bc2b8be0619fe649b461b
tryfilter <61294210+tryfilter@users.noreply.github.com>
2024-01-31 17:17:01 +0200

    Enable publishing core lib to local .m2 using 'gradle publishToMavenLocal'

--- lib-core/build.gradle.kts
+++ lib-core/build.gradle.kts
@@ -1,6 +1,19 @@
 plugins {
     id("convention-java")
+    id("maven-publish")
 }
 
 group = "org.example.nitpeek"
-version = "unspecified"
+version = "0.0"
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = "core"
+            version = project.version.toString()
+
+            from(components["java"])
+        }
+    }
+}

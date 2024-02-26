d32d812e26cb583d740ee4c4eaecde9908079bff
tryfilter <61294210+tryfilter@users.noreply.github.com>
2024-02-26 16:32:43 +0200

    Make lib-io a full JPMS module again (app had broken unnoticed)

---
+++ lib-io/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module nitpeek.lib.io {
+    requires nitpeek.lib.core;
+    requires nitpeek.lib.collection;
+    requires org.apache.pdfbox;
+    requires org.apache.pdfbox.io;
+    requires org.slf4j;
+
+    requires jdk.unsupported; // more complex workarounds may be required in the future. See app/build.gradle.kts prior to this commit.
+    requires org.docx4j.core;
+    requires org.docx4j.JAXB_ReferenceImpl;
+    requires jakarta.xml.bind;
+    requires org.checkerframework.checker.qual;
+
+    exports nitpeek.io.pdf;
+    exports nitpeek.io.pdf.convenience;
+    exports nitpeek.io.docx;
+}

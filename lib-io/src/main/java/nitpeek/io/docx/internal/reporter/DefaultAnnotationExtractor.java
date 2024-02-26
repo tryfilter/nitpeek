b53eccde1092d9facf80255f04ca446e3ff76df9
tryfilter <61294210+tryfilter@users.noreply.github.com>
2024-02-26 17:01:28 +0200

    Extraction of renderable annotations from DOCX documents

---
+++ lib-io/src/main/java/nitpeek/io/docx/internal/reporter/DefaultAnnotationExtractor.java
@@ -0,0 +1,37 @@
+package nitpeek.io.docx.internal.reporter;
+
+import nitpeek.core.api.common.Feature;
+import nitpeek.core.api.common.FeatureComponent;
+import nitpeek.core.api.translate.Translation;
+
+import java.util.Collection;
+import java.util.List;
+
+final class DefaultAnnotationExtractor implements AnnotationExtractor {
+
+    private final TextSelectionTransformer textSelectionTransformer;
+    private final String author;
+
+    public DefaultAnnotationExtractor(TextSelectionTransformer textSelectionTransformer, String author) {
+        this.textSelectionTransformer = textSelectionTransformer;
+        this.author = author;
+    }
+
+    @Override
+    public List<RenderableAnnotation> extractAnnotations(List<Feature> features, Translation i18n) {
+        return features
+                .stream()
+                .map(Feature::getComponents)
+                .flatMap(Collection::stream)
+                .map(featureComponent -> new RenderableAnnotation(getMessage(featureComponent, i18n), getTextSelection(featureComponent)))
+                .toList();
+    }
+
+    private DocxTextSelection getTextSelection(FeatureComponent featureComponent) {
+        return textSelectionTransformer.transform(featureComponent.getCoordinates());
+    }
+
+    private Message getMessage(FeatureComponent featureComponent, Translation i18n) {
+        return new Message(featureComponent.getDescription(i18n), author);
+    }
+}

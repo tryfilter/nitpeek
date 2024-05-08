43c605788183f978dba8880c268ffb6259307624
tryfilter <61294210+tryfilter@users.noreply.github.com>
2024-05-08 12:31:15 +0300

    Rename parameter to more generic version in interface

--- lib-io/src/main/java/nitpeek/io/SimpleAnnotator.java
+++ lib-io/src/main/java/nitpeek/io/SimpleAnnotator.java
@@ -8,7 +8,7 @@ public interface SimpleAnnotator {
 
     String ANNOTATED_PREFIX = "nitpicked_";
 
-    void annotateFeatures(Path inputPdf, Path outputDirectory) throws ReportingException;
+    void annotateFeatures(Path inputFile, Path outputDirectory) throws ReportingException;
 
     static Path outputPath(Path inputFile, Path outputDirectory) {
         return outputDirectory.resolve(outputDirectory.getFileSystem().getPath(ANNOTATED_PREFIX + inputFile.getFileName()));
